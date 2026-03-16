aa064e45d125509f76b3824be77906f53550583a
athiraajay <athiraajay2005@gmail.com>
2026-03-16 09:34:41 +0530

    UC1: Application Entry & Welcome Message

---
+++ src/BookMyStayApp.java
@@ -0,0 +1,20 @@
+/**
+ * =============================================================================
+ * MAIN CLASS - BookMyStayApp
+ * =============================================================================
+ * Use Case 1: Application Entry & Welcome Message
+ * @author Developer
+ * @version 1.0
+ */
+public class BookMyStayApp {
+
+    /**
+     * Application entry point.
+     * @param args Command-line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Hotel Booking Management System");
+        System.out.println("Hotel Booking System v1.0");
+        System.out.println("System has started successfully.");
+    }
+}
